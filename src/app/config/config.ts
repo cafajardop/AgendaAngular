ceb3c39e1cd95b83351d66a62462fd64e2595813
cafajardop <carlosafp84@hotmail.com>
2020-05-05 12:35:43 -0500

    Configuracion IP

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,3 @@
 //export const URL_SERVICIOS = 'https://localhost:44352';
-export const URL_SERVICIOS = 'http://localhost:8085';
+//export const URL_SERVICIOS = 'http://localhost:8085';
+export const URL_SERVICIOS = 'http://192.168.0.2:8085';
